9aa04fccd179e79d61cbac2df17e82b9f09e34a0
Brian Teague <br.teague@gmail.com>
2025-04-30 16:17:44 -0400

    Change to basic int type when comparing

--- FFIX/vivi_jump_rope.py
+++ FFIX/vivi_jump_rope.py
@@ -10,15 +10,15 @@ pixel_y = 625
 # Define 1x1 capture region
 monitor = {"top": pixel_y, "left": pixel_x, "width": 1, "height": 1}
 
-STARTING_GROUND_COLOR = (np.int64(143), np.int64(39), np.int64(23))
-ON_GROUND_COLOR = (np.int64(143), np.int64(42), np.int64(24))  # Replace with actual color if needed
-IN_AIR_COLOR = (np.int64(162), np.int64(129), np.int64(84))
+STARTING_GROUND_COLOR = (143, 39, 23)
+ON_GROUND_COLOR = (143, 42, 24)  # Replace with actual color if needed
+IN_AIR_COLOR = (162,129,84)
 
 def get_pixel_color():
     with mss.mss() as sct:
         img = sct.grab(monitor)
         pixel = np.array(img.pixel(0, 0))  # returns BGRA
-        return (pixel[0], pixel[1], pixel[2])  # Convert to RGB
+        return tuple(map(int, (pixel[0], pixel[1], pixel[2])))  # Convert to RGB
 
 def main():
     print("Starting fast pixel color detection. Ctrl+C to stop. Must initiate the first jump manually.")
@@ -27,6 +27,7 @@ def main():
 
     while True:
         color = get_pixel_color()
+        # print(f"Current color: {color}")
         if color == IN_AIR_COLOR:
             starting_to_land = True
         else:
@@ -34,6 +35,7 @@ def main():
                 pyautogui.press('enter')
                 time.sleep(0.005) #Prevents hitting enter too fast
                 starting_to_land = False
+
 
 if __name__ == "__main__":
     main()
